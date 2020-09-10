aa023d2584e6f3ec8f5c3c20a2b204ebcf15e98b
IngaKirsona <ingas@inbox.lv>
2020-09-10 18:35:47 +0300

    added error handling, delete alert

--- ShoppingList/ShoppingTableViewController.swift
+++ ShoppingList/ShoppingTableViewController.swift
@@ -12,11 +12,7 @@ import CoreData
 class ShoppingTableViewController: UITableViewController {
     
     var groceries = [Grocery]()
-    //------->create temporary grocery
-    //    var gros = [String]()
-    
     var managedObjectContext: NSManagedObjectContext?
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,26 +26,23 @@ class ShoppingTableViewController: UITableViewController {
         pickNewGrocery()
     }
     
-    
     func pickNewGrocery(){
         let alertController = UIAlertController(title: "Grocery Item!", message: "What do you want to buy?", preferredStyle: .alert)
         alertController.addTextField { (textField: UITextField) in
         }
         let addAction = UIAlertAction(title: "Add", style: .cancel) { (action: UIAlertAction) in
             let textField = alertController.textFields?.first
-            //            self.gros.append(textField!.text!)
-            //            self.tableView.reloadData()
             let entity = NSEntityDescription.entity(forEntityName: "Grocery", in: self.managedObjectContext!)
             let grocery = NSManagedObject(entity: entity!, insertInto: self.managedObjectContext)
             //-------> to add text to item
             grocery.setValue(textField?.text, forKey: "item")
-            //-------> to save text to Grocery
+//-------> to save text to Grocery
             do {
                 try self.managedObjectContext?.save()
             }catch{
                 fatalError("Error to store Grocery item")
             }
-            //-------> nedd to load because it reloads tableview
+//-------> nedd to load because it reloads tableview
             self.loadData()
         }//end addAction
         let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
@@ -58,7 +51,7 @@ class ShoppingTableViewController: UITableViewController {
         present(alertController, animated: true)
     }
     
-    //-------> to fetch and reload a tableview
+//-------> to fetch and reload a tableview
     func loadData(){
         let request: NSFetchRequest<Grocery> = Grocery.fetchRequest()
         do{
@@ -70,69 +63,53 @@ class ShoppingTableViewController: UITableViewController {
         }
     }
     
-    // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return groceries.count
     }
-    
-    
+//-------> cell formatting
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "shoppingCell", for: indexPath)
-        
-        //        cell.textLabel?.text = gros[indexPath.row]
         let grocery = groceries[indexPath.row]
         cell.textLabel?.text = grocery.value(forKey: "item") as? String
         cell.selectionStyle = .none
         cell.accessoryType = grocery.completed ? .checkmark : .none
-        
         return cell
     }
     
-    
-    /*
-     // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-    
-    //-------> to deselect checkmark
+//-------> to deselect checkmark
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         groceries[indexPath.row].completed = !groceries[indexPath.row].completed
+        errorHandling()
+    }
+//-------> to add delete option and alert
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let alert = UIAlertController(title: "Are you sure you want to delete?", message: nil, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            alert.addAction(UIAlertAction(title: "Delete", style: .default, handler: { (_) in
+                self.managedObjectContext?.delete(self.groceries[indexPath.row])
+                UIView.transition(with: tableView, duration: 1.0, options: .transitionCurlUp, animations: {
+                    self.loadData()
+                }, completion: nil)
+            }))
+            self.present(alert, animated: true)}
+        errorHandling()
+    }
+    
+//-------> to add an animation to cells
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+        UIView.animate(withDuration: 0.9){
+            cell.transform = CGAffineTransform.identity
+        }
+    }
+    func errorHandling(){
         do {
             try self.managedObjectContext?.save()
         }catch{
             fatalError("Error in deleting item")
         }
         loadData()
-    }
-    //-------> to add delete option and alert
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            let alert = UIAlertController(title: "Are you sure you want to delete?", message: nil, preferredStyle: .alert)
-                       alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-                       alert.addAction(UIAlertAction(title: "Delete", style: .default, handler: { (_) in
-                        self.managedObjectContext?.delete(self.groceries[indexPath.row])
-                        UIView.transition(with: tableView, duration: 1.0, options: .transitionCurlUp, animations: {
-                            self.loadData()
-                        }, completion: nil)
-                       }))
-            self.present(alert, animated: true)}
-        do {
-              try self.managedObjectContext?.save()
-          }catch{
-              fatalError("Error in deleting item")
-          }
-    }
-    
-    //-------> to add an animation to cells
-    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        cell.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
-        UIView.animate(withDuration: 0.8){
-            cell.transform = CGAffineTransform.identity
-        }
     }
 }
